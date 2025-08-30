d4c601ead4b5b2b76718703351c5dc888e5c0d74
renata <renata_castillo_@outlook.com>
2025-08-30 15:23:03 -0600

    1 commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,20 @@
 const config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '1.5rem',
+        lg: '2rem'
+      }
+    }
+  },
+  
   plugins: ["@tailwindcss/postcss"],
 };
 

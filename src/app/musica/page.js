3733a12f9c93ac4bf813ea2201584c209407f657
renata <renata_castillo_@outlook.com>
2025-11-17 21:35:06 -0600

    new cafes

---
+++ src/app/musica/page.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+
+
+export default  function Musica(){
+ 
+    return(
+        <div className='relative min-h-screen w-full overflow-hidden bg-gradient-to-bl from-[#3325a8] to-[#FFFFFF]'>
+              <div className="absolute  pointer-events-none bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#0000001a_1px,transparent_1px),linear-gradient(to_bottom,#0000001a_1px,transparent_1px)] bg-[size:20px_20px] "></div>
+            <div className="absolute  pointer-events-none bottom-0 left-0 right-0 top-0 bg-[radial-gradient(125%_125%_at_50%_10%,rgba(249,173,44,0.3)_40%,rgba(248,250,252,1)_100%)]"></div>            
+            
+        </div>
+      
+     
+    )
+}

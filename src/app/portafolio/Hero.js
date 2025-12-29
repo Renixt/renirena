3b2719ec4b9354040f13be3044da0b7483417bfc
renata <renata_castillo_@outlook.com>
2025-12-29 11:02:49 -0600

    portafolio p1

---
+++ src/app/portafolio/Hero.js
@@ -0,0 +1,49 @@
+import {
+  IconMapPinFilled,
+  IconPointerFilled,
+  IconStarFilled
+} from '@tabler/icons-react'
+export default function Hero() {
+  return (
+    <div className='mx-auto w-fit'>
+      <div className='flex flex-row gap-11'>
+        <div className='flex items-center border-2 border-y-0 border-r-0 border-l-0 pl-11 transition-transform ease-in-out hover:scale-105 hover:rotate-12'>
+          <p className='font-mansalva -rotate-45 text-7xl font-extrabold text-gray-800'>
+            '<span className='text-[#da6d6d]'>´</span>
+          </p>
+          <img
+            src='/imagenes/me2.png'
+            className='h-55 transition-transform ease-in-out hover:scale-105 hover:-rotate-12'
+          ></img>
+          <p className='font-mansalva mb-10 rotate-45 text-7xl font-extrabold text-gray-800'>
+            '<span className='text-[#da6d6d]'>´</span>
+          </p>
+        </div>
+
+        <div className='info border-2 border-y-0 border-r-0 border-l-gray-400 px-11 py-16'>
+          <h1 className='font-mansalva text-7xl font-extrabold text-gray-800'>
+            Renata Castillo
+          </h1>
+          <div className='mt-2 ml-4'>
+            <div className='mb-1 flex items-center gap-4 transition-transform hover:scale-101'>
+              <IconStarFilled size={20} color='#1F2937' />
+              <h2 className='text-gray-700'>Software Engineer</h2>
+            </div>
+            <div className='mb-1 flex items-center gap-4 transition-transform hover:scale-101'>
+              <IconMapPinFilled size={20} color='#1F2937' />
+              <h3 className='text-gray-700'>Queretaro, México</h3>
+            </div>
+
+            <div className='mb-1 flex items-start gap-4 transition-transform hover:scale-101'>
+              <IconPointerFilled size={20} color='#1F2937' />
+              <h3 className='leading-tight text-gray-700'>
+                Looking for intership opportunities{' '}
+                <span className='block'> and open to relocation. </span>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/portafolio/page.js
+++ src/app/portafolio/page.js
@@ -1,7 +1,35 @@
+import './styles.css'
+import Hero from './Hero'
+import {
+  IconBrandGithubFilled,
+  IconBrandLinkedinFilled,
+  IconFileCvFilled
+} from '@tabler/icons-react'
 export default function portafolio() {
   return (
-    <div>
-      <h1>Coming soon...</h1>
+    <div className='font-source-code-pro portafolio-bg min-h-screen overflow-hidden'>
+      <div className='absolute left-20 -ml-0.5 h-screen w-0.5 bg-[#da6d6d]'></div>
+      <div className='mx-5 my-5 flex justify-end-safe gap-2'>
+        <IconBrandLinkedinFilled
+          size={30}
+          color='#1F2937'
+          className='transition-transform hover:scale-105 hover:cursor-pointer hover:fill-[#2d516f]'
+        />
+        <IconBrandGithubFilled
+          size={30}
+          color='#1F2937'
+          className='transition-transform hover:scale-105 hover:cursor-pointer hover:fill-[#2d496f]'
+        />
+        <IconFileCvFilled
+          size={30}
+          color='#1F2937'
+          className='transition-transform hover:scale-105 hover:cursor-pointer hover:fill-[#2d496f]'
+        />
+      </div>
+      <hr className='border-gray-400' />
+
+      <Hero />
+      <hr className='border-gray-400' />
     </div>
   )
 }

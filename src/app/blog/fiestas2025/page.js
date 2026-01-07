d1f88af284777aa1dcd48fc3b258aad9f71ac065
renata <renata_castillo_@outlook.com>
2026-01-06 18:15:39 -0600

    Navidad familiar

--- src/app/blog/fiestas2025/page.js
+++ src/app/blog/fiestas2025/page.js
@@ -42,6 +42,49 @@ export default function page() {
     }
   ]
 
+  const galeria = [
+    {
+      img: '/fiestas2025/IM000031.JPG',
+      desc: 'Fiestas 2025 – Abrazo'
+    },
+    {
+      img: '/fiestas2025/IM000032.JPG',
+      desc: 'Fiestas 2025 – Naty recibe cuaderno de dibujo'
+    },
+    {
+      img: '/fiestas2025/IM000033.JPG',
+      desc: 'Fiestas 2025 – Tia erica y su baileys'
+    },
+    {
+      img: '/fiestas2025/IM000034.JPG',
+      desc: 'Fiestas 2025 – Roxana con calcetines nuevos'
+    },
+    {
+      img: '/fiestas2025/IM000035.JPG',
+      desc: 'Fiestas 2025 – Aniz del Mono'
+    },
+    {
+      img: '/fiestas2025/IM000036.JPG',
+      desc: 'Fiestas 2025 – Nueva cartera para la abuela'
+    },
+    {
+      img: '/fiestas2025/IM000037.JPG',
+      desc: 'Fiestas 2025 – Un gran regalo tejido'
+    },
+    {
+      img: '/fiestas2025/IM000038.JPG',
+      desc: 'Fiestas 2025 – Parece alcohol pero es un shampoo'
+    },
+    {
+      img: '/fiestas2025/IM000049.JPG',
+      desc: 'Fiestas 2025 – Imagen adicional'
+    },
+    {
+      img: '/fiestas2025/IM000050.JPG',
+      desc: 'Fiestas 2025 – Imagen adicional'
+    }
+  ]
+
   return (
     <div className='bg-amber-100'>
       <Navbar />
@@ -111,7 +154,7 @@ export default function page() {
           <h2 className='w-fit bg-amber-100 p-2 font-serif text-xl font-extrabold text-amber-900'>
             EL INTERCAMBIO
           </h2>
-          <div className='flex flex-wrap'>
+          <div className='flex flex-wrap justify-center'>
             {intercambio.map((foto, key) => (
               <div className='group relative' key={key}>
                 <img src={foto.img} className='w-90'></img>
@@ -122,22 +165,42 @@ export default function page() {
             ))}
           </div>
         </div>
+        <div className='my-10 flex flex-col flex-wrap shadow-md md:flex-row'>
+          {' '}
+          <div className='flex flex-col items-center justify-center bg-[#e6ae60] p-10 font-serif'>
+            <h2 className='w-fit p-2 text-xl font-extrabold text-amber-900'>
+              EL RESUMEN
+            </h2>
+            <div>
+              <p className='px-5 text-center text-amber-900 md:w-2xl'>
+                Gran navidad donde la mesa de niños y adultos fue fusionada por
+                primera vez en una misma. 2025 fue el inicio de ojala nuevas
+                tradiciones divertidas para las siguientes navidades. <br /> Se
+                jugaron diversos juegos para competir en equipo, donde salieron
+                a la luz estrategias tramposas, buenas ideas y sobre todo risas
+                y gritos. <br /> Con un menu digno de los mejores restaurantes,
+                cenamos todos juntos, un poco apretados en el nuevo espacio y
+                hambrientos despues de competir en equipos.
+              </p>
+            </div>
+          </div>
+          <img src='/fiestas2025/cats.png' className='m-10 h-80 md:m-0'></img>
+        </div>
 
-        <div className='my-10 flex flex-col items-center justify-center font-serif md:mx-auto md:max-w-3/4'>
-          <h2 className='w-fit bg-amber-100 p-2 text-xl font-extrabold text-amber-900'>
-            EL RESUMEN
+        {/*GALERIA */}
+        <div className='c-back flex w-full flex-col items-center justify-center border-4 border-amber-900 p-10'>
+          <h2 className='w-fit bg-amber-100 p-2 font-serif text-xl font-extrabold text-amber-900'>
+            GALERIA
           </h2>
-          <div>
-            <p className='px-5 text-center text-amber-900 md:w-2xl'>
-              Gran navidad donde la mesa de niños y adultos fue fusionada por
-              primera vez en una misma. 2025 fue el inicio de ojala nuevas
-              tradiciones divertidas para las siguientes navidades. <br /> Se
-              jugaron diversos juegos para competir en equipo, donde salieron a
-              la luz estrategias tramposas, buenas ideas y sobre todo risas y
-              gritos. <br /> Con un menu digno de los mejores restaurantes,
-              cenamos todos juntos, un poco apretados en el nuevo espacio y
-              hambrientos despues de competir en equipos.
-            </p>
+          <div className='container flex flex-wrap justify-center'>
+            {galeria.map((foto, key) => (
+              <div className='group relative' key={key}>
+                <img src={foto.img} className='h-50 md:h-96'></img>
+                <p className='absolute bottom-0 left-0 p-1 font-serif text-white opacity-0 transition-opacity duration-300 ease-in-out'>
+                  {foto.desc}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>

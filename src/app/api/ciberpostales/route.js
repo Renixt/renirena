ee6959c59de8a2e79d4995cb82ec8abfff10f1a0
renata <renata_castillo_@outlook.com>
2026-02-02 21:48:09 -0600

    ciberpostal outlook

--- src/app/api/ciberpostales/route.js
+++ src/app/api/ciberpostales/route.js
@@ -14,15 +14,17 @@ export async function POST(request) {
       )
     }
     const { data, error } = await resend.emails.send({
-      from: 'renata.castillo@codenok.com',
+      from: 'Renata <onboarding@resend.dev>',
       replyTo: from,
       to: to,
       subject: subject,
       html: `<div>
-        <h1>tqm naty</h1>    
+        <h1>${subject}</h1>    
+        <p>${message}</p>
         <img src="https://i.pinimg.com/originals/34/19/00/34190085b86b14d8de9b2eb7600bee00.gif"></img>
       </div>`
     })
+    console.log({ subject, message, to, from })
     return NextResponse.json({ data })
   } catch (error) {
     return NextResponse.json({ error: error.message })
